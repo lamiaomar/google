644f08f3c616358f50c8844c9ca92a7cf528302b
lamia <Lamia.Omar.ruh.k@tuwaiq.edu.sa>
2021-11-02 22:12:00 +0300

    initial commit

---
+++ app/src/main/java/com/example/google/MainActivity.kt
@@ -0,0 +1,35 @@
+package com.example.google
+
+import android.content.Context
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.KeyEvent
+import android.view.View
+import android.view.inputmethod.InputMethodManager
+import android.widget.Button
+import android.widget.ImageView
+import android.widget.Toast
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val rollButton: Button = findViewById(R.id.button)
+        rollButton.setOnClickListener {
+            val toast = Toast.makeText(this, "Register successfully", Toast.LENGTH_SHORT)
+            toast.show()
+        }
+
+    }
+    private fun handleKeyEvent(view: View, keyCode: Int): Boolean {
+        if (keyCode == KeyEvent.KEYCODE_ENTER) {
+            // Hide the keyboard
+            val inputMethodManager =
+                getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+            inputMethodManager.hideSoftInputFromWindow(view.windowToken, 0)
+            return true
+        }
+        return false
+    }
+}
